6c179e3cce6153bc75db4b958c107f13710e76f2
Priyanshu Kumar <116969540+prnshjh@users.noreply.github.com>
2025-03-18 16:02:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,19 @@ import Dashboard from "./Components/Dashboard";
 import TransactionHistory from "./Components/TransactionHistory";
 import Footer from "./Components/Footer";
 import AddTransaction from "./Components/AddTransaction";
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
 
 
 
 function App() {
   return (
     <div>
+       <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
       <Header/>
       <Hero/>
       <Dashboard/>
